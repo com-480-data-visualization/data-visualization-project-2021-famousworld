428592a61522acf85057575c0731c62526099660
Michał Bień <michal1.bien@gmail.com>
2021-04-04 00:56:31 +0200

    Remove log

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,13 @@
 // Stub from: https://gist.github.com/jfreels/6814721
 
-d3.text("dataset.csv", function(data) {
+d3.text("https://mbien-public.s3.eu-central-1.amazonaws.com/com-480/dataset.csv", function(data) {
     var parsedCSV = d3.csv.parseRows(data);
-    console.log("Dupa");
-    
+
     var container = d3.select("body")
         .append("table").selectAll("tr")
         .data(parsedCSV).enter()
         .append("tr").selectAll("td")
         .data(function(d) {
-            console.log(d);
             return d;
         }).enter()
         .append("td")
