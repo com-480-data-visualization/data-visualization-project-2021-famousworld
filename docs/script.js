0eb0f1c6e7a6f6e1de2c7f138e830b5c79f66319
Michał Bień <michal1.bien@gmail.com>
2021-04-04 00:13:40 +0200

    Improved js

--- docs/script.js
+++ docs/script.js
@@ -1,36 +1,21 @@
 // Stub from: https://gist.github.com/jfreels/6814721
 
-var tabulate = function (data,columns) {
-    var table = d3.select('body').append('table')
-    var thead = table.append('thead')
-    var tbody = table.append('tbody')
+d3.text("https://mbien-public.s3.eu-central-1.amazonaws.com/com-480/dataset.csv", function(data) {
+    var parsedCSV = d3.csv.parseRows(data);
 
-    thead.append('tr')
-    .selectAll('th')
-        .data(columns)
-        .enter()
-    .append('th')
-        .text(function (d) { return d })
+    var container = d3.select("body")
+        .append("table")
 
-    var rows = tbody.selectAll('tr')
-        .data(data)
-        .enter()
-    .append('tr')
+    .selectAll("tr")
+        .data(parsedCSV).enter()
+        .append("tr")
 
-    var cells = rows.selectAll('td')
-        .data(function(row) {
-            return columns.map(function (column) {
-                return { column: column, value: row[column] }
-        })
-    })
-    .enter()
-    .append('td')
-    .text(function (d) { return d.value })
-
-return table;
-}
-  
-d3.csv('https://mbien-public.s3.eu-central-1.amazonaws.com/com-480/dataset.csv', function (data) {
-    var columns = ['name','occupation','bplace_name']
-    tabulate(data,columns)
-})
+    .selectAll("td")
+        .data(function(d) {
+            return d;
+        }).enter()
+        .append("td")
+        .text(function(d) {
+            return d;
+        });
+});
