d9a2330aa6470d37ea6be698700f1edd1b9eead7
Michał Bień <michal1.bien@gmail.com>
2021-05-28 18:22:45 +0200

    More bulletproof script, another missing page fixed

--- src/process.py
+++ src/process.py
@@ -16,12 +16,20 @@ logger = logging.getLogger('dl_project')
 
 def find_commons_image(ids):
     """Query wikidata for the images describing the people."""
-    query = "SELECT ?item ?pic WHERE { VALUES ?item {" + ' '.join(ids) + "}. ?item wdt:P18 ?pic}"
-    result = requests.get("https://query.wikidata.org/sparql",
-                          params={'query': query},
-                          headers={'Accept': 'text/csv'}).text
-    data = list(csv.reader(result.split("\r\n"), delimiter=',', quotechar='"'))[:-1]
-    df = pd.DataFrame(data[1:], columns=data[0])
+    finished = False
+    while not finished:
+        try:
+            query = "SELECT ?item ?pic WHERE { VALUES ?item {" + ' '.join(ids) + "}. ?item wdt:P18 ?pic}"
+            result = requests.get("https://query.wikidata.org/sparql",
+                                  params={'query': query},
+                                  headers={'Accept': 'text/csv'}).text
+            data = list(csv.reader(result.split("\r\n"), delimiter=',', quotechar='"'))[:-1]
+            df = pd.DataFrame(data[1:], columns=data[0])
+            finished = True
+        except Exception:
+            print("It broke again... Resubmitting")
+            time.sleep(5)
+            continue
     return df
 
 
@@ -30,13 +38,13 @@ if __name__ == "__main__":
     df = pd.read_csv("https://storage.googleapis.com/pantheon-public-data/person_2020_update.csv.bz2")
 
     # Only individuals
-    df = df[df.is_group==False]
+    df = df[~df.is_group]
     df.drop(["prob_ratio", "gender", "twitter", "alive",
-            "l_", "l", "age", "coefficient_of_variation", "slug",
-            "birthdate", "deathdate", "bplace_geonameid",
-            "dplace_geonameid", "dplace_geacron_name",
-            "dplace_lat", "dplace_lon", "bplace_geacron_name", "non_en_page_views",
-            "hpi_raw", "is_group"],
+             "l_", "l", "age", "coefficient_of_variation", "slug",
+             "birthdate", "deathdate", "bplace_geonameid",
+             "dplace_geonameid", "dplace_geacron_name",
+             "dplace_lat", "dplace_lon", "bplace_geacron_name", "non_en_page_views",
+             "hpi_raw", "is_group"],
             axis=1, inplace=True)
 
     # Required fields
@@ -46,6 +54,7 @@ if __name__ == "__main__":
 
     # Weird bug in data
     df.loc[2116, "wp_id"] = 65527371
+    df.loc[3842, "wp_id"] = 67200122
 
     logger.info("Getting corresponding images...")
     subdfs = []
